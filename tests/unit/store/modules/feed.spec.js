1d19c9b4c743fcde93a5407d677e1aa152fcaf14
Leonardo Brito <f.leobrito@gmail.com>
2019-06-26 23:58:47 -0300

    test: feed action with mock

--- tests/unit/store/modules/feed.spec.js
+++ tests/unit/store/modules/feed.spec.js
@@ -1,4 +1,7 @@
 import Vuex from 'vuex';
+import sinon from 'sinon';
+import MockAdapter from 'axios-mock-adapter';
+import api from '@/services/api';
 import { createLocalVue } from '@vue/test-utils';
 import feed from '@/store/modules/feed';
 
@@ -13,11 +16,24 @@ const fetchedPosts = [
   },
 ];
 
+const mock = new MockAdapter(api);
+
 describe('Feed module', () => {
   it('should mutate on feed', () => {
     const { FETCH_FEED } = feed.mutations;
     FETCH_FEED(feed.state, { posts: fetchedPosts });
     const { posts } = feed.state;
     expect(posts.length).toBe(1);
+  });
+
+  it('should fetch feed through action fetchFeed', async () => {
+    mock.onGet('/posts').reply(200, fetchedPosts);
+    const { fetchFeed } = feed.actions;
+    const { state } = feed;
+    const commit = sinon.spy();
+    await fetchFeed({ commit, state });
+    const { posts } = feed.state;
+    expect(posts.length).toBe(1);
+    expect(posts).toEqual(fetchedPosts);
   });
 });
